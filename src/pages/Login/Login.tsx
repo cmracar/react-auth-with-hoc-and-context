0dfbe13f86ac3e272cf95e273a4d2d55dc19ca6c
Cemre ACAR <cemreacar94@gmail.com>
2022-05-03 00:45:08 +0300

    update form placeholder

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -23,8 +23,8 @@ const Login = () => {
                         <h2>Login Page</h2>
                     </div>
                     <div className="form">
-                        <input type="text" onChange={(e) => setFormData({ ...formData, username: e.target.value })} defaultValue={formData?.username} name="username" />
-                        <input type="password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} defaultValue={formData?.password} name="password" />
+                        <input type="text" onChange={(e) => setFormData({ ...formData, username: e.target.value })} placeholder={"Username"} defaultValue={formData?.username} name="username" />
+                        <input type="password" onChange={(e) => setFormData({ ...formData, password: e.target.value })} placeholder={"Password"} defaultValue={formData?.password} name="password" />
                         <button onClick={handleLogin}>Login</button>
                     </div>
                 </div>
